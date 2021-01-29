a211d444c416846dd626d5dbca2451901976f6c1
Prasanth <kesavarapu.siva@gmail.com>
2021-01-29 23:01:50 +0530

    html template for those who need

--- curlify/index.ts
+++ curlify/index.ts
@@ -1,12 +1,29 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import fs = require('fs');
+
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    var slash = '\\';
+    const double = '\\\\'
+    const isHtmlResponse = (req.headers['accept'] ?? '').includes('text/html');
+    if (isHtmlResponse) slash = double;
     var comment = '';
     var headers = '';
-    var data = '# if you modifying, request payload make sure to drop content-length or update to correct value';
+    var data = '';
     const bodymethods = ['POST', 'PUT']
-    if (req.body && bodymethods.indexOf(req.method) === -1) {
-        comment = comment + `\n# as per spec, its adviced to have body for ${bodymethods} requests`
+    if (req.body) {
+        comment = `# if you modifying, request payload make sure to drop content-length 
+    # or update to correct value`
+        if (bodymethods.indexOf(req.method) === -1)
+            comment = comment + `\n# as per spec, its adviced to have body for ${bodymethods} requests`
+    }
+    if (req.headers) {
+        Object.keys(req.headers).forEach(key => {
+            headers = `-H "${key}: ${req.headers[key]}" ${slash}\n`
+        });
+        if (headers !== '') {
+            headers = headers.substring(0, headers.length);
+        }
     }
     if (req.body) {
         if (req.rawBody.indexOf('"') == -1) {
@@ -16,34 +33,25 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         } else {
             data = `--data '''${req.rawBody}'''`
         }
+        var body = `curl "${req.url}" ${slash}
+    -X ${req.method} ${slash}
+    ${headers}    ${data} ${slash}
+${comment}`
+    } else {
+        var body = `curl "${req.url}" ${slash}
+    -X ${req.method} ${slash}
+    ${headers}    ${comment}`
     }
-    if (req.headers) {
-        Object.keys(req.headers).forEach(key => {
-            headers = `-H "${key}: ${req.headers[key]}" \\\n`
-        });
-        if (headers !== '') {
-            headers = headers.substring(0, headers.length - 2);
-        }
-    }
-    var body = `curl "${req.url}" \\
-    -X ${req.method} \\
-    ${headers}`
-    if (data !== '') {
-        body = body + '\\\n' + data;
-    }
-    if (comment !== '') {
-        body = body + '\\\n' + comment;
-    }
-    const isHtmlResponse = false
+
+    context.res.headers = { 'authoredby': 'https://github.com/cedric05', };
     if (isHtmlResponse) {
-        context.log('not yet implemented'); // TODO
+        context.res.headers = { 'content-type': 'text/html' }
+        const data = fs.readFileSync('./template.html');
+        body = data.toString().replace('CURLSTATEMENT', body);
+    } else {
+        context.res.headers = { 'content-type': 'text/plain' }
     }
-    context.res = {
-        'headers': {
-            'authoredby': 'https://github.com/cedric05',
-        },
-        'body': body
-    }
+    context.res.body = body;
 };
 
 export default httpTrigger;
